36c772de8c0a9a4f084fa7a76820524d2f7eb234
jesus-acm <crispinjesus97@gmail.com>
2021-08-02 13:45:14 -0500

    Promesas y callbacks

---
+++ 05-Promesas-Callbacks/src/index.js
@@ -0,0 +1,17 @@
+import { heroeIfAwait, heroesCiclo, obtenerHeroeAwait, obtenerHeroesArr } from './js/await';
+import './style.css';
+
+// import { buscarHeroe, buscarHeroeAsync, promesaLenta, promesaMedia, promesaRapida } from './js/promesas';
+
+// promesaLenta.then(console.log)
+// promesaMedia.then(console.log)
+// promesaRapida.then(console.log)
+
+// buscarHeroe('capi').then(console.log);
+// buscarHeroeAsync('iron').then(console.log);
+
+// obtenerHeroesArr().then(console.log)
+
+heroesCiclo()
+
+heroeIfAwait('iron')
